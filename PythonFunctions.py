1e755f1385cbf811c6c60bf657ac90836d392da9
meetbrahmbhatt91 <meetbrahmbhatt91@gmail.com>
2025-08-12 13:31:17 +0530

    Added Positional Argument function and Arbitrary Keyword function

--- PythonFunctions.py
+++ PythonFunctions.py
@@ -1,4 +1,27 @@
-def fun():
-    print("Welcome to My Repo")
+# This function is with type_hints
+def evenOdd(x: int) -> str:
+    if (x % 2 == 0):
+        return "Even"
+    else:
+        return "Odd"
+
+# This function is with Position arguments. 
+def nameAge(name, age):
+    print("Hi, I am ", name)
+    print("My age is ", age)
     
-fun()
+print("Case-1:")
+nameAge("Suraj", 29)
+print("\nCase-2:")
+nameAge(21, "Suraj")
+
+# Arbitrary Keyword Arguments
+# In Python Arbitrary Keyword Arguments, *args, and **kwargs can pass a variable number of 
+# arguments to a function using special symbols. There are two special symbols:
+# *args in Python (Non-Keyword Arguments)
+# **kwargs in Python (Keyword Arguments)
+def myFunc(*args):
+    for arg in args:
+        print(arg)
+
+myFunc("Hello", "My", "Name", "is", "Meet")
